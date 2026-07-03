3fb95f996c5592488d969fa2157962be687a456b
vsawantvinay <vsawant.azure@gmail.com>
2026-07-03 18:46:27 +0530

    Jenkins main.tf and install.sh added

---
+++ install.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+sudo apt update
+sudo apt install fontconfig openjdk-21-jre -y
+java -version
+sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2026.key
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt update
+sudo apt install jenkins maven -y
+apt install openjdk-21-jdk-headless -y
+sudo systemctl start jenkins
+
+
+### AWS CLI install
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+apt install unzip -y
+unzip awscliv2.zip
+sudo ./aws/install
+
+### Install DOCKER
+
+# Add Docker's official GPG key:
+sudo apt update
+sudo apt install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/ubuntu
+Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
+Components: stable
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+
+sudo apt update
+
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+sudo usermod -aG docker jenkins
+sudo systemctl restart jenkins
+sudo systemctl restart docker
+
+### install kubectl
+cd /opt
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+
+############## terrafom
+
+wget -O - https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(grep -oP '(?<=UBUNTU_CODENAME=).*' /etc/os-release || lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update && sudo apt install terraform
+
+########### eksctl
+
---
+++ main.tf
@@ -0,0 +1,35 @@
+provider "aws" {
+
+  region = "ap-south-1"
+
+}
+
+ 
+
+resource "aws_instance" "jenkins-vm" {
+
+  ami = "ami-05d2d839d4f73aafb"
+
+  instance_type = "m7i-flex.large"
+
+  key_name = "practicekey"
+
+  user_data = file("install.sh")
+
+  root_block_device {
+
+    volume_size = 50
+
+    delete_on_termination = true
+
+  }
+
+ 
+
+  tags = {
+
+    Name = "jenkins-server"
+
+  }
+
+}
